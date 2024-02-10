e24a86145d6f3f76e92de42fcbae55e24d0abea5
Brian Fernando <132426362+BrianFdo03@users.noreply.github.com>
2024-02-10 09:10:48 +0530

    Add files via upload

---
+++ Lab 3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("Goosebumps", "R.L Stine");
+            Book book2 = new Book("Harry Potter", "J.K. Rowling");
+
+            Console.WriteLine("Detils of book 1");
+            Console.WriteLine(book1.Title);
+            Console.WriteLine(book1.Author);
+
+            Console.WriteLine("Detils of book 2");
+            Console.WriteLine(book2.Title);
+            Console.WriteLine(book2.Author);
+
+
+        }
+    }
+
+    class Book
+    {
+        public string Title;
+        public string Author;
+
+        public Book(string Title , string Author)
+        {
+            this.Title = Title;
+            this.Author = Author;
+        }
+    }
+}
