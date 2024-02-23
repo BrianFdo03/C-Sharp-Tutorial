b7af086fa751603b8bee05dc5526827e6d43ad7c
Brian Fernando <132426362+BrianFdo03@users.noreply.github.com>
2024-02-23 09:25:06 +0530

    Add files via upload

---
+++ Lab 4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameOfCourse;
+            double mark;
+            string instructorName;
+
+            bool check = true;
+            do
+            {
+                Console.WriteLine("Enter name of Course");
+                nameOfCourse = Console.ReadLine();
+                Console.WriteLine();
+
+                if (nameOfCourse == "" || nameOfCourse == " ")
+                {
+                    Console.WriteLine("ERROR Course Name cannot be not Empty. Re-enter Course Name");
+                }
+                else
+                {
+                    check = false;
+                }
+
+            } while (check == true);
+
+
+            check = true;
+            do
+            {
+                Console.WriteLine("Enter the relevant grade for that course");
+                mark = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine();
+
+                if (mark > 100 || mark < 0)
+                {
+                    Console.WriteLine("ERROR Grade should be with 1 - 100. Re-enter Grade");
+                }
+                else
+                {
+                    check = false;
+                }
+
+            } while (check == true);
+
+
+            check = true;
+            do
+            {
+                Console.WriteLine("Enter relavant instructor name");
+                instructorName = Console.ReadLine();
+                Console.WriteLine();
+
+                if (instructorName == "" || instructorName == " ")
+                {
+                    Console.WriteLine("ERROR Instructor Name cannot be empty. Re-enter Instructor Name");
+                }
+                else
+                {
+                    check = false;
+                }
+
+            } while (check == true);
+
+
+            Course course = new Course(nameOfCourse, mark, instructorName);
+
+            Console.WriteLine();
+            course.PrintCourseInfo();
+        }
+    }
+
+    class Course
+    {
+        private string courseName;
+        private string instructorName;
+        private double grade;
+        
+        public Course(string courseName, double grade, string teacherName)
+        {
+            this.CourseName = courseName;
+            this.Grade = grade;
+            SetInstructorName(teacherName);
+        }
+
+        public string CourseName
+        {
+            get { return courseName; }
+            
+            private set
+            {
+                courseName = value;
+            }
+        }
+
+        public double Grade
+        {
+            get { return grade; }
+            
+            private set
+            {
+                grade = value;
+            }
+        }
+
+        public void SetInstructorName(string teacherName)
+        {
+                instructorName = teacherName;
+        }
+
+        private string CalculateLetterGrade()
+        {
+            if (grade >= 90)
+            {
+                return "A";
+            }
+            else if (grade >= 80)
+            {
+                return "B";
+            }
+            else if (grade >= 70)
+            {
+                return "C";
+            }
+            else if (grade >= 60)
+            {
+                return "D";
+            }
+            else if (grade >= 50)
+            {
+                return "E";
+            }
+            else
+            {
+                return "F";
+            }
+        }
+
+        public void PrintCourseInfo()
+        {
+                Console.WriteLine($"Course Name: {courseName}");
+                Console.WriteLine($"instructor Name : {instructorName}");
+                Console.WriteLine($"Course Grade: {CalculateLetterGrade()}");
+        }
+
+    }
+}
---
+++ Lab 4/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // a)	Create an instance of the "Employee" class, representing an employee with an ID of 101, full name "John Doe," and a salary of 50000.
+            Employee employee = new Employee(101, "John Doe", 50000);
+
+            Console.WriteLine();
+            // b)	Access and display the employee ID using the read-only property.
+            Console.WriteLine($"Employee ID: { employee.employeeID}");
+
+            Console.WriteLine();
+            // c)	Update the full name of the employee using the read-write property.
+            Console.WriteLine("Changing name from John to Joe");
+            employee.FullName = "Joe Doe";
+            Console.WriteLine();
+            Console.WriteLine($"Employee Name: { employee.FullName}");
+
+            Console.WriteLine();
+            // d)	Attempt to modify the salary directly from external code (outside the class). Observe and comment on the result.
+            // employee.salary = 10000;
+            // Gives an error saying salary isn't inside employee since we have made it private it shows that error
+
+            employee.DisplayEmployeeInfo();
+
+        }
+    }
+    class Employee
+    {
+        public int employeeID;
+        public string FullName;
+        private double Salary;
+
+        public int empID
+        {
+            get
+            {
+                return employeeID;
+            }
+            private set
+            {
+                employeeID = value; 
+            }
+        }
+
+        public string empFullName
+        {
+            get
+            {
+                return FullName;
+            }
+            set
+            {
+                FullName = value;
+            }
+        }
+
+
+        public Employee(int employeeID, string FullName, double Salary)
+        {
+            this.empID = employeeID;
+            this.empFullName = FullName;
+            this.Salary = Salary;
+        }
+
+        public double getSalary()
+        {
+            return Salary;
+        }
+
+        public void DisplayEmployeeInfo()
+        {
+            Console.WriteLine("Details of employee\n");
+            Console.WriteLine($"Employee Name: {FullName}");
+            Console.WriteLine($"Employee ID: {employeeID}");
+            Console.WriteLine($"Emplyee Saary: {getSalary()}");
+        }
+
+    }
+}
