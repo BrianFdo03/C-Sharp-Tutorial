09e40f505034198d297a33f5acc35a9235176e57
Brian Fernando <132426362+BrianFdo03@users.noreply.github.com>
2024-02-23 13:37:13 +0530

    Add files via upload

---
+++ Lab 5/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // a)	Create an instance of the "Product" class, representing a product with an ID of 101, name "Laptop," price $800, and quantity in stock 10.
+            Product product = new Product(101, "Laptop", 800, 10);
+
+            // b)	Access and display the name, price, and quantity of the product.
+            Console.WriteLine();
+            Console.WriteLine("Product Name " + product.productName);
+            Console.WriteLine("Product Price " + product.price);
+            Console.WriteLine("Product Quantity Available " + product.quantityInStock);
+
+            // c)	Attempt to modify the product's ID from external code (outside the class). Observe and comment on the result.
+            Console.WriteLine();
+            // product.productId = 105;
+            // Error stating productId not in the context of class Product. This is because productId is a private variable.
+
+            product.BuyProduct("Laptop", 2);
+
+        }
+    }
+
+    class Product
+    {
+        private int productId;
+        public string productName;
+        public double price;
+        public int quantityInStock;
+
+
+
+        public Product(int productId, string productName, double price, int quantityInStock)
+        {
+            this.productId = productId;
+            this.productName = productName;
+            this.price = price;
+            this.quantityInStock = quantityInStock;
+        }
+
+        public int getProductId()
+        {
+            return productId;
+        }
+
+        public void AddProduct(string productName, int amountOfProducts)
+        {
+            if (amountOfProducts < 0 )
+            {
+                Console.WriteLine("Amount of products should be more than zero");
+            }
+            else
+            {
+                quantityInStock += amountOfProducts;
+                Console.WriteLine($"Added {amountOfProducts} units of {productName} to the inventory.");
+                Console.WriteLine("Currently inventory has " + quantityInStock + " units of " + productName);
+            }
+        }
+
+        public void BuyProduct(string productName, int amountOfProducts)
+        {
+            double totalPrice;
+            if (amountOfProducts < 0)
+            {
+                Console.WriteLine("Amount of products needed to be purchased should be more than zero");
+            }
+            else if (amountOfProducts > quantityInStock)
+            {
+                Console.WriteLine($"Cannot purchase this amount. Only {quantityInStock} {productName} are in stock");
+            }
+            else
+            {
+                Console.WriteLine("Price of 1 " + productName + " is Rs. " + price);
+                totalPrice = price * amountOfProducts;
+                Console.WriteLine("Amount to be paid Rs. " + totalPrice );
+                Console.WriteLine($"Bought {amountOfProducts} units of {productName} from the store.");
+                quantityInStock -= amountOfProducts;
+                Console.WriteLine("Currently inventory has " + quantityInStock + " units of " + productName);
+            }
+        }
+    }
+}
---
+++ Lab 5/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // a)	Create an instance of the Rectangle class representing a rectangle with a length of 5 and a width of 3.
+            Rectangle rectangle = new Rectangle(5, 3);
+
+            // b)	Display the shape information for the rectangle.
+            Console.WriteLine();
+            rectangle.DisplayShapeInfo();
+
+            // c)	Create an instance of the Circle class representing a circle with a radius of 4.
+            Console.WriteLine();
+            Circle circle = new Circle(4);
+
+            // d)	Display the shape information for the circle.
+            Console.WriteLine();
+            circle.DisplayShapeInfo();
+
+        }
+    }
+
+    class Shape
+    {
+        public string ShapeType;
+        public double Area;
+
+        public Shape()
+        {
+            this.ShapeType = "Shape";
+            Area = 0;
+        }
+
+        public virtual void CalculateArea(double val1, double val2)
+        {
+
+        }
+
+        public virtual void DisplayShapeInfo()
+        {
+            Console.WriteLine("Shape type : " + ShapeType);
+            Console.WriteLine("Shape area : " + Area);
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        double length;
+        double width;
+
+        public Rectangle(double length, double width)
+        {
+           ShapeType = "Rectangle";
+           SetDimensions(length, width);
+        }
+
+        public void SetDimensions(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+            CalculateArea(length, width);
+        }
+
+        public override void CalculateArea(double val1, double val2)
+        {
+            Area = val1 * val2;
+            // Console.WriteLine("Area of Rectangle : " + Area + " square units");
+        }
+
+        public override void DisplayShapeInfo()
+        {
+            Console.WriteLine("Shape Length " + length);
+            Console.WriteLine("Shape Width " + width);
+            base.DisplayShapeInfo();
+        }
+    }
+
+    class Circle : Shape
+    {
+        double radius;
+
+        public Circle(double radius)
+        {
+            ShapeType = "Circle";
+            SetRadius(radius);
+        }
+
+        public void SetRadius(double radi)
+        {
+            this.radius = radi;
+            CalculateArea(radi);
+        }
+
+        public void CalculateArea(double radi)
+        {
+            Area = 3.14 * radi * radi;
+            // Console.WriteLine("Area of Circle : " + Area + " square units");
+        }
+
+        public override void DisplayShapeInfo()
+        {
+            Console.WriteLine("Shape Radius " + radius);
+            base.DisplayShapeInfo();
+        }
+    }
+}
