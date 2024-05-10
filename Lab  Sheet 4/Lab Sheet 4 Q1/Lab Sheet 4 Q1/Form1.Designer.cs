84d64c4fa1ed6c7553794978dddbabb96825b860
Brian Fernando <132426362+BrianFdo03@users.noreply.github.com>
2024-05-10 06:30:34 +0530

    Q1

---
+++ Lab  Sheet 4/Lab Sheet 4 Q1/Lab Sheet 4 Q1/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace Lab_Sheet_4_Q1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxInput1 = new System.Windows.Forms.TextBox();
+            this.txtBoxInput2 = new System.Windows.Forms.TextBox();
+            this.lblInput1 = new System.Windows.Forms.Label();
+            this.lblInput2 = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBoxInput1
+            // 
+            this.txtBoxInput1.Location = new System.Drawing.Point(262, 133);
+            this.txtBoxInput1.Name = "txtBoxInput1";
+            this.txtBoxInput1.Size = new System.Drawing.Size(158, 20);
+            this.txtBoxInput1.TabIndex = 0;
+            // 
+            // txtBoxInput2
+            // 
+            this.txtBoxInput2.Location = new System.Drawing.Point(262, 183);
+            this.txtBoxInput2.Name = "txtBoxInput2";
+            this.txtBoxInput2.Size = new System.Drawing.Size(158, 20);
+            this.txtBoxInput2.TabIndex = 1;
+            // 
+            // lblInput1
+            // 
+            this.lblInput1.AutoSize = true;
+            this.lblInput1.Location = new System.Drawing.Point(133, 133);
+            this.lblInput1.Name = "lblInput1";
+            this.lblInput1.Size = new System.Drawing.Size(40, 13);
+            this.lblInput1.TabIndex = 2;
+            this.lblInput1.Text = "Input 1";
+            // 
+            // lblInput2
+            // 
+            this.lblInput2.AutoSize = true;
+            this.lblInput2.Location = new System.Drawing.Point(133, 183);
+            this.lblInput2.Name = "lblInput2";
+            this.lblInput2.Size = new System.Drawing.Size(40, 13);
+            this.lblInput2.TabIndex = 3;
+            this.lblInput2.Text = "Input 2";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutput.Location = new System.Drawing.Point(211, 34);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(82, 25);
+            this.lblOutput.TabIndex = 4;
+            this.lblOutput.Text = "Output";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(379, 279);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.lblInput2);
+            this.Controls.Add(this.lblInput1);
+            this.Controls.Add(this.txtBoxInput2);
+            this.Controls.Add(this.txtBoxInput1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoxInput1;
+        private System.Windows.Forms.TextBox txtBoxInput2;
+        private System.Windows.Forms.Label lblInput1;
+        private System.Windows.Forms.Label lblInput2;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Button btnAdd;
+    }
+}
+
