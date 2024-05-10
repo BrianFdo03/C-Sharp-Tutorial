6b03e4eb93c4b30c47e62aac9c001ed4f8c504e8
Brian Fernando <132426362+BrianFdo03@users.noreply.github.com>
2024-05-10 07:54:46 +0530

    Q3

---
+++ Lab  Sheet 4/Lab Sheet 4 Q3/Lab Sheet 4 Q3/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace Lab_Sheet_4_Q3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxInput1 = new System.Windows.Forms.TextBox();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSubstract = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.btnEqual = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBoxInput1
+            // 
+            this.txtBoxInput1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxInput1.Location = new System.Drawing.Point(131, 133);
+            this.txtBoxInput1.Name = "txtBoxInput1";
+            this.txtBoxInput1.Size = new System.Drawing.Size(289, 29);
+            this.txtBoxInput1.TabIndex = 0;
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutput.Location = new System.Drawing.Point(211, 34);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(82, 25);
+            this.lblOutput.TabIndex = 4;
+            this.lblOutput.Text = "Output";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(369, 219);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSubstract
+            // 
+            this.btnSubstract.Location = new System.Drawing.Point(278, 219);
+            this.btnSubstract.Name = "btnSubstract";
+            this.btnSubstract.Size = new System.Drawing.Size(75, 23);
+            this.btnSubstract.TabIndex = 6;
+            this.btnSubstract.Text = "Substract";
+            this.btnSubstract.UseVisualStyleBackColor = true;
+            this.btnSubstract.Click += new System.EventHandler(this.btnSubstract_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Location = new System.Drawing.Point(187, 219);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(75, 23);
+            this.btnMultiply.TabIndex = 7;
+            this.btnMultiply.Text = "Multiply";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Location = new System.Drawing.Point(97, 219);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(75, 23);
+            this.btnDivide.TabIndex = 8;
+            this.btnDivide.Text = "Divide";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // btnEqual
+            // 
+            this.btnEqual.Location = new System.Drawing.Point(218, 287);
+            this.btnEqual.Name = "btnEqual";
+            this.btnEqual.Size = new System.Drawing.Size(75, 23);
+            this.btnEqual.TabIndex = 9;
+            this.btnEqual.Text = "Equal";
+            this.btnEqual.UseVisualStyleBackColor = true;
+            this.btnEqual.Click += new System.EventHandler(this.btnEqual_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEqual);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnSubstract);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.txtBoxInput1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoxInput1;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSubstract;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Button btnEqual;
+    }
+}
---
+++ Lab  Sheet 4/Lab Sheet 4 Q3/Lab Sheet 4 Q3/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_Sheet_4_Q3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool canCalculate = false;
+        decimal output, tempNum = 0;
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            decimal input1 = decimal.Parse(txtBoxInput1.Text);
+
+            if (tempNum == 0)
+            {
+                lblOutput.Text = $"{input1} + ";
+                tempNum = input1;
+                txtBoxInput1.Clear();
+                txtBoxInput1.Focus();
+            }
+            else if (tempNum != 0 && canCalculate == false)
+            {
+                lblOutput.Text += input1.ToString() + " ";
+                canCalculate = true;
+                
+            }
+            else
+            {
+                MessageBox.Show("2 inputs already added", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void btnSubstract_Click(object sender, EventArgs e)
+        {
+            int input1 = int.Parse(txtBoxInput1.Text);
+
+            if (tempNum == 0)
+            {
+                lblOutput.Text = $"{input1} - ";
+                tempNum = input1;
+                txtBoxInput1.Clear();
+                txtBoxInput1.Focus();
+            }
+            else if (tempNum != 0 && canCalculate == false)
+            {
+                lblOutput.Text += input1.ToString() + " ";
+                canCalculate = true;
+            }
+            else
+            {
+                MessageBox.Show("2 inputs already added", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            int input1 = int.Parse(txtBoxInput1.Text);
+
+            if (tempNum == 0)
+            {
+                lblOutput.Text = $"{input1} * ";
+                tempNum = input1;
+                txtBoxInput1.Clear();
+                txtBoxInput1.Focus();
+            }
+            else if (tempNum != 0 && canCalculate == false)
+            {
+                lblOutput.Text += input1.ToString() + " ";
+                canCalculate = true;
+            }
+            else
+            {
+                MessageBox.Show("2 inputs already added", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            int input1 = int.Parse(txtBoxInput1.Text);
+
+            if (tempNum == 0)
+            {
+                lblOutput.Text = $"{input1} / ";
+                tempNum = input1;
+                txtBoxInput1.Clear();
+                txtBoxInput1.Focus();
+            }
+            else if (tempNum != 0 && canCalculate == false)
+            {
+                lblOutput.Text += input1.ToString() + " ";
+                canCalculate = true;
+            }
+            else
+            {
+                MessageBox.Show("2 inputs already added", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            if (canCalculate != true)
+            {
+                MessageBox.Show("2 inputs should be inserted to calculate", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else if (lblOutput.Text.Contains("+"))
+            {
+                output = tempNum + Convert.ToInt32(txtBoxInput1.Text);
+                tempNum = 0;
+                canCalculate = false;
+                txtBoxInput1.Clear();
+            }
+            else if (lblOutput.Text.Contains("-"))
+            {
+                output = Convert.ToInt32(txtBoxInput1.Text) - tempNum;
+                tempNum = 0;
+                canCalculate = false;
+                txtBoxInput1.Clear();
+            }
+            else if (lblOutput.Text.Contains("*"))
+            {
+                output = tempNum * Convert.ToInt32(txtBoxInput1.Text);
+                tempNum = 0;
+                canCalculate = false;
+                txtBoxInput1.Clear();
+            }
+            else
+            {
+                output = tempNum / Convert.ToDecimal(txtBoxInput1.Text);
+                tempNum = 0;
+                canCalculate = false;
+                txtBoxInput1.Clear();
+            }
+
+            lblOutput.Text += "= " + output;
+        }
+    }
+}
